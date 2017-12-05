0df749488d58efd7a25bb65fac0a8fc2825758da
Max King <maxdavidking@gmail.com>
2017-12-05 11:18:31 -0600

    Add resourceful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :sushi
+  resources :user
+  resources :about
+  root to: 'about#index'
 end
