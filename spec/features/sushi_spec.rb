3f70f4d7e1c4039ea17cf296dae3ab3a33a2814b
Max King <maxdavidking@gmail.com>
2017-12-05 11:18:56 -0600

    Add test for list all Sushi connections feature

--- spec/features/sushi_spec.rb
+++ spec/features/sushi_spec.rb
@@ -4,8 +4,12 @@ RSpec.describe "Sushi feature" do
   describe "list all of the sushi connections for each user" do
     it "lists sushi connection information" do
       #1. create your test data
+      Sushi.create!(name: "jstor", endpoint: "https://www.jstor.org/sushi", cust_id: "iit.edu", req_id: "galvinlib", report_start: "2016-01-01", report_end: "2016-12-31", password: "")
       #2. interact with the app
-      #3. expect something to happen 
+      visit('/')
+      click_link('Sushi Connections')
+      #3. expect something to happen
+      expect(page).to have_content('jstor')
     end
   end
 end
