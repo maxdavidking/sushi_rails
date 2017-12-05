60067e8f4211ab1bb726e269379d8aae35f6ef26
Max King <maxdavidking@gmail.com>
2017-12-05 10:26:13 -0600

    Add outline of features tests

---
+++ spec/features/sushi_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "Sushi feature" do
+  describe "list all of the sushi connections for each user" do
+    it "lists sushi connection information" do
+      #1. create your test data
+      #2. interact with the app
+      #3. expect something to happen 
+    end
+  end
+end
