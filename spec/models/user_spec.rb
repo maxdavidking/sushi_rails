38bc10b8948d6fae7a4b81e6ae5629a34073b739
Max King <maxdavidking@gmail.com>
2017-12-05 09:57:55 -0600

    Add validation test for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject{described_class.new(name: "Max King", organization: "IIT")}
+
+  describe 'validations' do
+    describe 'validate name field' do
+      it 'must be present' do
+        expect(subject).to be_valid
+        subject.name = nil
+        expect(subject).to_not be_valid
+      end
+    end
+  end
 end
