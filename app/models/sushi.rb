9e3ff87788664ae6d61ae9847e4c9e7ec6c90e09
Max King <maxdavidking@gmail.com>
2017-12-05 10:07:12 -0600

    Add tests for Sushi model validation, tests passing

--- app/models/sushi.rb
+++ app/models/sushi.rb
@@ -1,3 +1,8 @@
 class Sushi < ApplicationRecord
   validates :name, presence: true
+  validates :endpoint, presence: true
+  validates :cust_id, presence: true
+  validates :req_id, presence: true
+  validates :report_start, presence: true
+  validates :report_end, presence: true
 end
--- spec/models/sushi_spec.rb
+++ spec/models/sushi_spec.rb
@@ -7,10 +7,50 @@ RSpec.describe Sushi, type: :model do
   describe 'validations' do
     describe 'validate name field' do
       it 'must be present' do
-        expect(subject).to be_valid
+        expect(subject).to be_valid(:name)
 
         subject.name = nil
         expect(subject).to_not be_valid(:name)
+      end
+    end
+    describe 'validate endpoint field' do
+      it 'must be present' do
+        expect(subject).to be_valid(:endpoint)
+
+        subject.endpoint = nil
+        expect(subject).to_not be_valid(:endpoint)
+      end
+    end
+    describe 'validate cust_id field' do
+      it 'must be present' do
+        expect(subject).to be_valid(:cust_id)
+
+        subject.cust_id = nil
+        expect(subject).to_not be_valid(:cust_id)
+      end
+    end
+    describe 'validate req_id field' do
+      it 'must be present' do
+        expect(subject).to be_valid(:req_id)
+
+        subject.req_id = nil
+        expect(subject).to_not be_valid(:req_id)
+      end
+    end
+    describe 'validate report_start field' do
+      it 'must be present' do
+        expect(subject).to be_valid(:report_start)
+
+        subject.report_start = nil
+        expect(subject).to_not be_valid(:report_start)
+      end
+    end
+    describe 'validate report_end field' do
+      it 'must be present' do
+        expect(subject).to be_valid(:report_end)
+
+        subject.report_end = nil
+        expect(subject).to_not be_valid(:report_end)
       end
     end
   end
