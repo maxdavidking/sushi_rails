a28bb6a46c9ab0b1d0475572dbdb9a817656e58d
Max King <maxdavidking@gmail.com>
2017-12-05 09:24:06 -0600

    Add Sushi model

---
+++ db/migrate/20171205152132_create_sushis.rb
@@ -0,0 +1,15 @@
+class CreateSushis < ActiveRecord::Migration[5.1]
+  def change
+    create_table :sushis do |t|
+      t.string :name
+      t.string :endpoint
+      t.string :cust_id
+      t.string :req_id
+      t.string :report_start
+      t.string :report_end
+      t.string :password
+
+      t.timestamps
+    end
+  end
+end
