656765fa218207c514ce9db6a1bdf6ba88cb1013
Max King <maxdavidking@gmail.com>
2017-12-06 10:28:42 -0600

    Add feature test to update user profile

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,4 +2,8 @@ class UserController < ApplicationController
   def index
     @user = User.all
   end
+  def edit
+    @user = User.find(params[:id])
+    @user.update(name: "Adam" )
+  end
 end
--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,19 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe "User features" do
+  let(:create_users) do
+    User.create!(name: "Max King", organization: "IIT")
+    User.create!(name: "test", organization: "test")
+  end
   let(:view_users) do
     visit('/')
     click_link('User Profile')
   end
   describe "List user information" do
     it "lists all user profiles" do
-      User.create!(name: "Max King", organization: "IIT")
-      User.create!(name: "test", organization: "test")
-
+      create_users
       view_users
-
       expect(page).to have_content('Max King')
+    end
+  end
 
+  describe "Edit user information" do
+    it "allows users to edit their profiles" do
+      create_users
+      view_users
+      first(:link, 'Edit Profile').click
+      expect(page).to have_content('Adam')
     end
   end
 end
