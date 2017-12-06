6c8cadea0ee3209f5db9433eb28b9d1e268c9784
Max King <maxdavidking@gmail.com>
2017-12-06 11:53:49 -0600

    Add create user feature test, working code for that test

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,4 +6,17 @@ class UserController < ApplicationController
     @user = User.find(params[:id])
     @user.update(name: "Adam" )
   end
+  def new
+    @user = User.new
+  end
+  def create
+    user = User.new(user_params)
+    user.save
+    redirect_to('/user')
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :organization)
+  end
 end
--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -9,11 +9,26 @@ RSpec.describe "User features" do
     visit('/')
     click_link('User Profile')
   end
+
   describe "List user information" do
     it "lists all user profiles" do
       create_users
       view_users
       expect(page).to have_content('Max King')
+    end
+  end
+  describe "Add user information" do
+    it "allows users to create a profile" do
+      user = User.create!(name: "Brian", organization: "IIT")
+
+      view_users
+      click_link('Create Profile')
+      fill_in 'Name', with: user.name
+      fill_in 'Organization', with: user.organization
+      click_on('Create')
+      visit('/user')
+
+      expect(page).to have_content "#{user.name}"
     end
   end
 
@@ -22,6 +37,7 @@ RSpec.describe "User features" do
       create_users
       view_users
       first(:link, 'Edit Profile').click
+      #fill_in 'Name', with: "Adam"
       expect(page).to have_content('Adam')
     end
   end
