ee165fc53f8f76661bc1e1d239c0a21239585583
Max King <maxdavidking@gmail.com>
2017-12-06 09:25:32 -0600

    Add let for reusable step

--- spec/features/sushi_spec.rb
+++ spec/features/sushi_spec.rb
@@ -1,14 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "Sushi feature" do
+  let(:view_sushi_connections) do
+    visit('/')
+    click_link('Sushi Connections')
+  end
   describe "list all of the sushi connections for each user" do
     it "lists sushi connection information" do
-      #1. create your test data
+
       Sushi.create!(name: "jstor", endpoint: "https://www.jstor.org/sushi", cust_id: "iit.edu", req_id: "galvinlib", report_start: "2016-01-01", report_end: "2016-12-31", password: "")
-      #2. interact with the app
-      visit('/')
-      click_link('Sushi Connections')
-      #3. expect something to happen
+      Sushi.create!(name: "acm", endpoint: "http://sushi4.scholarlyiq.com/SushiService.svc", cust_id: "X124552", req_id: "90633e00-fc53-4f70-9ae0-ac2c33d00014", report_start: "2016-01-01", report_end: "2016-12-31", password: "")
+
+      view_sushi_connections
+
       expect(page).to have_content('jstor')
     end
   end
