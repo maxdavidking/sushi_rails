a4b1c7f0bf3faae40f0428b1a391f795e222f537
Max King <maxdavidking@gmail.com>
2017-12-06 09:26:25 -0600

    Add let to users feature test

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,13 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "User features" do
+  let(:view_users) do
+    visit('/')
+    click_link('User Profile')
+  end
   describe "List user information" do
     it "lists all user profiles" do
       User.create!(name: "Max King", organization: "IIT")
       User.create!(name: "test", organization: "test")
 
-      visit('/')
-      click_link('User Profile')
+      view_users
 
       expect(page).to have_content('Max King')
 
