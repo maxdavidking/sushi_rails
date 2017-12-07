f56a4ad3fb070a0eb701b30bf94d9e2b880f802e
Max King <maxdavidking@gmail.com>
2017-12-07 10:08:06 -0600

    Add validations for new columns in User model:

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   validates :name, presence: true
   validates :organization, presence: true
+  validates :uid, presence: true
+  validates :provider, presence: true
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject{described_class.new(name: "Max King", organization: "IIT")}
+  subject{described_class.new(name: "Max King", organization: "IIT", uid: "01101001111", provider: "Google")}
 
   describe 'validations' do
     describe 'validate name field' do
@@ -18,6 +18,22 @@ RSpec.describe User, type: :model do
 
         subject.organization = nil
         expect(subject).to_not be_valid(:organization)
+      end
+    end
+    describe 'validate provider field' do
+      it 'must be present' do
+        expect(subject).to be_valid(:provider)
+
+        subject.provider = nil
+        expect(subject).to_not be_valid(:provider)
+      end
+    end
+    describe 'validate uid field' do
+      it 'must be present' do
+        expect(subject).to be_valid(:uid)
+
+        subject.uid = nil
+        expect(subject).to_not be_valid(:uid)
       end
     end
   end
