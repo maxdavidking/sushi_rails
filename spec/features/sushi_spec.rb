5c463fe6f2c21bb9ca8e31d8a5184260b7d268da
Max King <maxdavidking@gmail.com>
2017-12-11 15:21:10 -0600

    Update tests for active record association requirements

--- spec/features/sushi_spec.rb
+++ spec/features/sushi_spec.rb
@@ -1,16 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "Sushi feature" do
+  let(:create_user_sushi) do
+    @user = User.create!(id: 100, name: "Brian", organization: "test", uid: "0001111", provider: "google")
+    Sushi.create!(name: "jstor", endpoint: "https://www.jstor.org/sushi", cust_id: "iit.edu", req_id: "galvinlib", report_start: "2016-01-01", report_end: "2016-12-31", password: "", user_id: @user.id)
+    Sushi.create!(name: "acm", endpoint: "http://sushi4.scholarlyiq.com/SushiService.svc", cust_id: "X124552", req_id: "90633e00-fc53-4f70-9ae0-ac2c33d00014", report_start: "2016-01-01", report_end: "2016-12-31", password: "", user_id: @user.id)
+  end
   let(:view_sushi_connections) do
     visit('/')
     click_link('Sushi Connections')
   end
   describe "list all of the sushi connections for each user" do
     it "lists sushi connection information" do
-
-      Sushi.create!(name: "jstor", endpoint: "https://www.jstor.org/sushi", cust_id: "iit.edu", req_id: "galvinlib", report_start: "2016-01-01", report_end: "2016-12-31", password: "")
-      Sushi.create!(name: "acm", endpoint: "http://sushi4.scholarlyiq.com/SushiService.svc", cust_id: "X124552", req_id: "90633e00-fc53-4f70-9ae0-ac2c33d00014", report_start: "2016-01-01", report_end: "2016-12-31", password: "")
-
+      create_user_sushi
       view_sushi_connections
 
       expect(page).to have_content('jstor')
@@ -19,7 +21,8 @@ RSpec.describe "Sushi feature" do
 
   describe "create new sushi connection" do
     it "creates a new sushi connection" do
-      conn = Sushi.create!(name: "science direct", endpoint: "http://sushi4.scholarlyiq.com/SushiService.svc", cust_id: "X124552", req_id: "90633e00-fc53-4f70-9ae0-ac2c33d00014", report_start: "2016-01-01", report_end: "2016-12-31", password: "")
+      @user = User.create!(id: 100, name: "Brian", organization: "test", uid: "0001111", provider: "google")
+      conn = Sushi.create!(name: "science direct", endpoint: "http://sushi4.scholarlyiq.com/SushiService.svc", cust_id: "X124552", req_id: "90633e00-fc53-4f70-9ae0-ac2c33d00014", report_start: "2016-01-01", report_end: "2016-12-31", password: "", user_id: @user.id)
 
       view_sushi_connections
       click_link("Create Sushi Connection")
@@ -38,8 +41,8 @@ RSpec.describe "Sushi feature" do
 
   describe "edit sushi connection information" do
     it "edits sushi connection information" do
-
-      connection = Sushi.create!(name: "science direct", endpoint: "http://sushi4.scholarlyiq.com/SushiService.svc", cust_id: "X124552", req_id: "90633e00-fc53-4f70-9ae0-ac2c33d00014", report_start: "2016-01-01", report_end: "2016-12-31", password: "")
+      @user = User.create!(id: 100, name: "Brian", organization: "test", uid: "0001111", provider: "google")
+      connection = Sushi.create!(name: "science direct", endpoint: "http://sushi4.scholarlyiq.com/SushiService.svc", cust_id: "X124552", req_id: "90633e00-fc53-4f70-9ae0-ac2c33d00014", report_start: "2016-01-01", report_end: "2016-12-31", password: "", user_id: @user.id)
 
       view_sushi_connections
       first(:link, 'Edit Connection').click
@@ -51,14 +54,13 @@ RSpec.describe "Sushi feature" do
   end
   describe "delete sushi connection information" do
     it "deletes sushi connection information" do
-      Sushi.create!(name: "science direct", endpoint: "https://www.jstor.org/sushi", cust_id: "iit.edu", req_id: "galvinlib", report_start: "2016-01-01", report_end: "2016-12-31", password: "")
-      Sushi.create!(name: "acm", endpoint: "http://sushi4.scholarlyiq.com/SushiService.svc", cust_id: "X124552", req_id: "90633e00-fc53-4f70-9ae0-ac2c33d00014", report_start: "2016-01-01", report_end: "2016-12-31", password: "")
+      create_user_sushi
 
       view_sushi_connections
-      expect(page).to have_content('science direct')
+      expect(page).to have_content('jstor')
       first(:link, 'Delete').click
 
-      expect(page).to_not have_content('science direct')
+      expect(page).to_not have_content('jstor')
     end
   end
 end
