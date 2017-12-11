7b6555bc57687911e97ac7a1e63fab9ec525e670
Max King <maxdavidking@gmail.com>
2017-12-11 13:37:31 -0600

    update model tests for shoulda_matchers syntax

--- spec/models/sushi_spec.rb
+++ spec/models/sushi_spec.rb
@@ -2,56 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Sushi, type: :model do
 
-  subject{described_class.new(name: "jstor", endpoint: "https://www.jstor.org/sushi", cust_id: "iit.edu", req_id: "galvinlib", report_start: "2016-01-01", report_end: "2016-12-31", password: "")}
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:endpoint) }
+  it { should validate_presence_of(:cust_id) }
+  it { should validate_presence_of(:req_id) }
+  it { should validate_presence_of(:report_start) }
+  it { should validate_presence_of(:report_end) }
 
-  describe 'validations' do
-    describe 'validate name field' do
-      it 'must be present' do
-        expect(subject).to be_valid(:name)
-
-        subject.name = nil
-        expect(subject).to_not be_valid(:name)
-      end
-    end
-    describe 'validate endpoint field' do
-      it 'must be present' do
-        expect(subject).to be_valid(:endpoint)
-
-        subject.endpoint = nil
-        expect(subject).to_not be_valid(:endpoint)
-      end
-    end
-    describe 'validate cust_id field' do
-      it 'must be present' do
-        expect(subject).to be_valid(:cust_id)
-
-        subject.cust_id = nil
-        expect(subject).to_not be_valid(:cust_id)
-      end
-    end
-    describe 'validate req_id field' do
-      it 'must be present' do
-        expect(subject).to be_valid(:req_id)
-
-        subject.req_id = nil
-        expect(subject).to_not be_valid(:req_id)
-      end
-    end
-    describe 'validate report_start field' do
-      it 'must be present' do
-        expect(subject).to be_valid(:report_start)
-
-        subject.report_start = nil
-        expect(subject).to_not be_valid(:report_start)
-      end
-    end
-    describe 'validate report_end field' do
-      it 'must be present' do
-        expect(subject).to be_valid(:report_end)
-
-        subject.report_end = nil
-        expect(subject).to_not be_valid(:report_end)
-      end
-    end
-  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,32 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject{described_class.new(name: "Max King", organization: "IIT", uid: "01101001111", provider: "Google")}
-
-  describe 'validations' do
-    describe 'validate name field' do
-      it 'must be present' do
-        expect(subject).to be_valid(:name)
-
-        subject.name = nil
-        expect(subject).to_not be_valid(:name)
-      end
-    end
-    describe 'validate provider field' do
-      it 'must be present' do
-        expect(subject).to be_valid(:provider)
-
-        subject.provider = nil
-        expect(subject).to_not be_valid(:provider)
-      end
-    end
-    describe 'validate uid field' do
-      it 'must be present' do
-        expect(subject).to be_valid(:uid)
-
-        subject.uid = nil
-        expect(subject).to_not be_valid(:uid)
-      end
-    end
-  end
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:provider) }
+  it { should validate_presence_of(:uid) }
 end
