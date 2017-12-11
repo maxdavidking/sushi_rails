981bf9d3fcfcc00dbbb3135f96ef72e332554d4b
Max King <maxdavidking@gmail.com>
2017-12-11 15:21:34 -0600

    Add shoulda_matcher for active record associations to user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,5 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:provider) }
   it { should validate_presence_of(:uid) }
+  it { should have_many(:sushi) }
 end
