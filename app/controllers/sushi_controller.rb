499abb3b7ec42b1903e07920b0dd315cb9461074
Max King <maxdavidking@gmail.com>
2018-01-02 15:18:05 -0600

    DRY out code by calling sushi_call from helper rather then from controller twice. Redirect to index on failure

--- app/controllers/sushi_controller.rb
+++ app/controllers/sushi_controller.rb
@@ -28,58 +28,7 @@ class SushiController < ApplicationController
 
   def test
     @sushi = Sushi.find(params[:id])
-    #Set up client for connection
-    client = Savon.client(
-    wsdl: "http://www.niso.org/schemas/sushi/counter_sushi4_0.wsdl",
-    endpoint: @sushi.endpoint,
-    #Possible namespaces required by WSDL
-    namespaces:{
-      "xmlns:tns" => "SushiService",
-      "xmlns:sc" => "http://www.niso.org/schemas/sushi/counter",
-      "xmlns:xsd" => "http://www.w3.org/2001/XMLSchema",
-      "xmlns:soapenv" => "http://schemas.xmlsoap.org/soap/envelope/",
-      "xmlns:sus" => "http://www.niso.org/schemas/sushi",
-      "xmlns:soap" => "http://schemas.xmlsoap.org/wsdl/soap",
-      "xmlns:soap12" => "http://schemas.xmlsoap.org/wsdl/soap12/"
-      },
-      convert_request_keys_to: :none,
-      #Log default is false, unless specified by user
-      logger: Rails.logger,
-      log: true,
-      log_level: :debug,
-      #Pretty Print is dependent on logging being on
-      pretty_print_xml: true,
-      env_namespace: :soapenv
-    )
-      #Send out call for data, passing all parameters for login/data
-    @response = client.call(:get_report, message: {
-      "sus:Requestor" => {
-        "sus:ID" => @sushi.req_id,
-        "sus:Name" => "Blank"
-        #"sus:Email" => "#{@options[:password]}"
-      },
-      "sus:CustomerReference" => {
-        "sus:ID" => @sushi.cust_id
-      },
-      "sus:ReportDefinition" => {
-        "sus:Filters" => {
-            "sus:UsageDateRange" => {
-            "sus:Begin" => @sushi.report_start,
-            "sus:End" => @sushi.report_end
-            },
-          },
-        },
-        :attributes! => {
-          "sus:ReportDefinition" => {
-            "Release" => "4",
-            "Name" => "JR1"
-          },
-        },
-      } )
-    rescue
-      @response = ""
-      #@file = File.open("raw_xml/#{@options[:customer]}-#{Time.now.strftime("%Y%m%d")}.xml", "w+")
-      #File.write(@file, xml.to_xml)
+    @response = helpers.sushi_call
   end
 
   def call
@@ -93,7 +42,8 @@ class SushiController < ApplicationController
         format.csv { send_data helpers.csv_open, filename: "users-#{Date.today}.csv" }
       end
     rescue
-      redirect_to action: "call", id: @sushi.id
+      redirect_to action: "index"
+      flash[:error] = "Failure, try testing your connection"
     end
   end
 
