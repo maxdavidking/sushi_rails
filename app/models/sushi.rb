489c270216fc552ccbed4a5c9d6ce4262623b0c6
Max King <maxdavidking@gmail.com>
2018-01-25 09:49:11 -0600

    remove requestor_id validation as not all vendors require that field

--- app/models/sushi.rb
+++ app/models/sushi.rb
@@ -3,7 +3,6 @@ class Sushi < ApplicationRecord
   validates :name, presence: true
   validates :endpoint, presence: true
   validates :cust_id, presence: true
-  validates :req_id, presence: true
   validates :report_start, presence: true
   validates :report_end, presence: true
   validates_format_of :report_start, :with => /\d{4}-\d{2}-\d{2}/, :message => "^Date must be in the following format: YYYY-MM-DD"
