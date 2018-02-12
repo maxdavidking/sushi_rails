da87c481932a5a9fc3728c6874137ee1e0f8a051
Max King <maxdavidking@gmail.com>
2018-02-12 09:56:00 -0600

    Update user tests to pass - new language for various buttons

--- spec/controllers/user_spec.rb
+++ spec/controllers/user_spec.rb
@@ -11,15 +11,16 @@ RSpec.describe "User features" do
   describe "User login actions", :type => :feature do
     it "lists logged in user's profile data" do
       sign_in
-      click_link "user"
+      click_link "User Profile"
       expect(page).to have_content('mockuser')
     end
     it "allows users to edit their profiles" do
       sign_in
+      click_link "User Profile"
       click_link "Edit Profile"
       expect(page).to have_content('Organization')
       fill_in 'Organization', :with => "IIT"
-      click_button "Edit"
+      click_button "Update"
       expect(page).to have_content('IIT')
     end
   end
