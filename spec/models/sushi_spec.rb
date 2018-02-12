03aaa4af8c72ae7b2b855369941ab119a7e2d460
Max King <maxdavidking@gmail.com>
2018-02-12 10:13:46 -0600

    New model tests for valdation

--- spec/models/sushi_spec.rb
+++ spec/models/sushi_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe Sushi, type: :model do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:endpoint) }
   it { should validate_presence_of(:cust_id) }
-  it { should validate_presence_of(:req_id) }
   it { should validate_presence_of(:report_start) }
   it { should validate_presence_of(:report_end) }
 
