165fe00e0f22f27a6dfbbc0cc0b9d0b363cbd597
Max King <maxdavidking@gmail.com>
2018-02-13 16:05:03 -0600

    Add rescue for login if login fails

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,14 @@
 class SessionsController < ApplicationController
   def create
-    @user = User.from_omniauth(request.env['omniauth.auth'])
-    session[:user_id] = @user.id
-    flash[:success] = "Welcome, #{@user.name}"
-    redirect_to root_path
+    begin
+      @user = User.from_omniauth(request.env['omniauth.auth'])
+      session[:user_id] = @user.id
+      flash[:success] = "Welcome, #{@user.name}"
+      redirect_to root_path
+    rescue
+      flash[:danger] = "FetchCounter only works with gmail accounts"
+      redirect_to root_path
+    end
   end
 
   def destroy
