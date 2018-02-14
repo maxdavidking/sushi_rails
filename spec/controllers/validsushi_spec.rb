3e899e0fbb3a59c749cff21a9b3953db01cdbe1b
Max King <maxdavidking@gmail.com>
2018-02-14 13:30:57 -0600

    Test importing validated connections

--- spec/controllers/validsushi_spec.rb
+++ spec/controllers/validsushi_spec.rb
@@ -5,18 +5,19 @@ RSpec.describe "ValidsushiController" do
     visit '/'
     mock_auth_hash
     click_link "Google"
+    Validsushi.create!(name: "jstor", endpoint: "https://www.jstor.org/sushi", cust_id: "YOUR_CUSTOMER_ID", req_id: "YOUR_REQUESTOR_ID", report_start: "2017-01-01", report_end: "2017-12-31", password: "OPTIONAL")
+    click_link("Valid COUNTER Connections")
   end
   describe "Valid Sushi features", :type => :feature do
     include ApplicationHelper
     it "Lists all valid Sushi connections" do
       sign_in
-      Validsushi.create!(name: "jstor", endpoint: "https://www.jstor.org/sushi", cust_id: "YOUR_CUSTOMER_ID", req_id: "YOUR_REQUESTOR_ID", report_start: "2017-01-01", report_end: "2017-12-31", password: "OPTIONAL")
-      click_link("Valid COUNTER Connections")
-      save_and_open_page
       expect(page).to have_content("jstor")
     end
     it "Imports valid sushi connections to user" do
       sign_in
+      click_link("Import")
+      expect(page).to have_content("jstor")
     end
   end
 end
