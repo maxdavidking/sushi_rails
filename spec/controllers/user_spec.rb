4df43a256fa4a8a4d9b8139a422b3347b51e8f39
Max King <maxdavidking@gmail.com>
2018-02-14 12:37:13 -0600

    remove test for editing organization - org field no longer listed per user

--- spec/controllers/user_spec.rb
+++ spec/controllers/user_spec.rb
@@ -14,14 +14,5 @@ RSpec.describe "User features" do
       click_link "User Profile"
       expect(page).to have_content('mockuser')
     end
-    it "allows users to edit their profiles" do
-      sign_in
-      click_link "User Profile"
-      click_link "Edit Profile"
-      expect(page).to have_content('Organization')
-      fill_in 'Organization', :with => "IIT"
-      click_button "Update"
-      expect(page).to have_content('IIT')
-    end
   end
 end
