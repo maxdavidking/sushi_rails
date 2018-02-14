e9a6f17e05c5b2e6e6ff30263fa83588a8e5ec2d
Max King <maxdavidking@gmail.com>
2018-02-14 15:33:19 -0600

    Add create and new methods for validsushi

--- app/controllers/validsushi_controller.rb
+++ app/controllers/validsushi_controller.rb
@@ -13,4 +13,23 @@ class ValidsushiController < ApplicationController
       redirect_to ('/validsushi')
     end
   end
+
+  def new
+    @validsushi = Validsushi.new
+  end
+
+  def create
+    @validsushi = Validsushi.new(validsushi_params)
+    if @validsushi.save
+      redirect_to('/validsushi')
+    else
+      flash[:danger] = "Error: #{@validsushi.errors.full_messages}"
+      redirect_to ('/validsushi/new')
+    end
+  end
+
+  def validsushi_params
+    params.require(:validsushi).permit(:name, :endpoint, :cust_id, :req_id, :report_start, :report_end, :password)
+  end
+
 end
