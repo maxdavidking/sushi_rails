07c3ee601142a9232d7c529006d1a161f23d4d77
Max King <maxdavidking@gmail.com>
2018-02-14 17:04:34 -0600

    Add success message and redirect back to import

--- app/controllers/validsushi_controller.rb
+++ app/controllers/validsushi_controller.rb
@@ -7,10 +7,11 @@ class ValidsushiController < ApplicationController
     @validsushi = Validsushi.find(params[:id])
     @sushi = Sushi.new(name: @validsushi.name, endpoint: @validsushi.endpoint, cust_id: @validsushi.cust_id, req_id: @validsushi.req_id, report_start: @validsushi.report_start, report_end: @validsushi.report_end, password: @validsushi.password, user_id: current_user.id )
     if @sushi.save
-      redirect_to('/sushi')
+      flash[:success] = "Added #{@validsushi.name}"
+      redirect_to("/validsushi")
     else
       flash[:danger] = "Error: #{@sushi.errors.full_messages}"
-      redirect_to ('/validsushi')
+      redirect_to("/validsushi")
     end
   end
 end
