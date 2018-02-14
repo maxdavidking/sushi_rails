c91427d4437fb43fbf7aa2dd8cc66622a2593010
Max King <maxdavidking@gmail.com>
2018-02-14 12:35:36 -0600

    Update model validations to pass model tests

--- app/models/validsushi.rb
+++ app/models/validsushi.rb
@@ -1,2 +1,7 @@
 class Validsushi < ApplicationRecord
+  validates :name, presence: true
+  validates :endpoint, presence: true
+  validates :cust_id, presence: true
+  validates :report_start, presence: true
+  validates :report_end, presence: true
 end
