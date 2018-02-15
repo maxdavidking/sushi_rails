225cf8d5cbd99096fcafbe511a7101b7a02c9d68
Max King <maxdavidking@gmail.com>
2018-02-15 12:33:04 -0600

    Add test for sushi validation on user_id and name combo

--- spec/models/sushi_spec.rb
+++ spec/models/sushi_spec.rb
@@ -7,5 +7,6 @@ RSpec.describe Sushi, type: :model do
   it { should validate_presence_of(:cust_id) }
   it { should validate_presence_of(:report_start) }
   it { should validate_presence_of(:report_end) }
+  it { should validate_uniqueness_of(:name).scoped_to(:user_id)}
 
 end
