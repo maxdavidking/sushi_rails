ecce7d1b4ada772d4d874b6699362f4b49a13592
Max King <maxdavidking@gmail.com>
2018-02-16 12:25:34 -0600

    remove user.url from OAuth - not needed

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,6 @@ class << self
     user.name = auth_hash['info']['name']
     user.location = auth_hash['info']['location']
     user.image_url = auth_hash['info']['image']
-    user.url = auth_hash['info']['urls']['Twitter']
     user.save!
     user
   end
