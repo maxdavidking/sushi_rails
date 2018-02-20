d36dcba886d929af1624e58343814041da75f8b6
Max King <maxdavidking@gmail.com>
2018-02-20 09:42:02 -0600

    Add Puma Worker Killer and config to kill puma workers when they hit too much memory - otherwise site will slow to crawl and crash

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'nprogress-rails'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.5'
 gem 'savon'
+gem 'puma_worker_killer'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,8 @@
+before_fork do
+  require 'puma_worker_killer'
+
+  PumaWorkerKiller.start
+end
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
@@ -21,7 +26,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
