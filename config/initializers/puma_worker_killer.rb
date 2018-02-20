3228e6b12b99fb76fb3e218c3633b5a8398119f9
Max King <maxdavidking@gmail.com>
2018-02-20 10:38:54 -0600

    Lower puma_worker_killer threshold

--- config/initializers/puma_worker_killer.rb
+++ config/initializers/puma_worker_killer.rb
@@ -1,7 +1,7 @@
 PumaWorkerKiller.config do |config|
   config.ram           = 4096 # mb
   config.frequency     = 5    # seconds
-  config.percent_usage = 0.50
+  config.percent_usage = 0.40
   config.rolling_restart_frequency = 12 * 3600 # 12 hours in seconds
   config.reaper_status_logs = true # setting this to false will not log lines like:
   # PumaWorkerKiller: Consuming 54.34765625 mb with master and 2 workers.
