f1983e3ea1892aa64cb8bf1503614e3276df861f
Max King <maxdavidking@gmail.com>
2018-03-09 13:50:28 -0600

    Add threading to main report call so that users can still navigate around site

--- app/controllers/sushi_controller.rb
+++ app/controllers/sushi_controller.rb
@@ -52,24 +52,33 @@ class SushiController < ApplicationController
 
   def call
     @sushi = Sushi.find(params[:id])
+    #Ensure user can only access their own connections
     unless session[:user_id] == @sushi.user_id
       flash[:danger] = "That's not your sushi connection"
       redirect_to root_path
       return
     end
     begin
-      helpers.sushi_call
-      helpers.months_math(@sushi.report_start, @sushi.report_end)
-      helpers.count_months
-      helpers.xml_open
-      helpers.get_secondary_data
-      helpers.get_item_data
-      helpers.get_total_data
-      respond_to do |format|
-        format.html
-        format.csv { send_data helpers.data_write("\,"), filename: "#{@sushi.name}-#{Date.today}.csv" }
-        format.tsv { send_data helpers.data_write("\t"), filename: "#{@sushi.name}-#{Date.today}.tsv" }
+      #Call this process in a new thread, as it can take a while.
+      thread = Thread.new do
+        #Not possible to deadlock in this case, but still best practice to wrap new thread code
+        Rails.application.executor.wrap do
+          helpers.sushi_call
+          helpers.months_math(@sushi.report_start, @sushi.report_end)
+          helpers.count_months
+          helpers.xml_open
+          helpers.get_secondary_data
+          helpers.get_item_data
+          helpers.get_total_data
+        end
       end
+      #Rejoin the completed thread to deliver content
+      thread.join
+        respond_to do |format|
+          format.html
+          format.csv { send_data helpers.data_write("\,"), filename: "#{@sushi.name}-#{Date.today}.csv" }
+          format.tsv { send_data helpers.data_write("\t"), filename: "#{@sushi.name}-#{Date.today}.tsv" }
+        end
     rescue
       redirect_to("/sushi")
       flash[:danger] = "Failure, try testing your connection"
