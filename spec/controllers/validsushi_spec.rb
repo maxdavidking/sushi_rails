09d8fca976cbac01aee7ab5028fdc9f09d8948b0
Max King <maxdavidking@gmail.com>
2018-03-16 12:36:48 -0500

    Fix validsushi tests to log in to correct link:

--- spec/controllers/validsushi_spec.rb
+++ spec/controllers/validsushi_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "ValidsushiController" do
   let (:sign_in) do
     visit '/'
     mock_auth_hash
-    click_link "Google"
+    click_link "Login"
     Validsushi.create!(name: "jstor", endpoint: "https://www.jstor.org/sushi", cust_id: "YOUR_CUSTOMER_ID", req_id: "YOUR_REQUESTOR_ID", report_start: "2017-01-01", report_end: "2017-12-31", password: "OPTIONAL")
     visit('/validsushi')
   end
