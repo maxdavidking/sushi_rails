2fec098f59985a6202e89dd530b5943af36128f2
Max King <maxdavidking@gmail.com>
2018-03-20 12:48:59 -0500

    Add default routes for organizations and data

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
       get ':id/call', to: 'sushi#call'
     end
   end
+  resources :organization
+  resources :datum 
   resources :user
   resources :about
   root to: 'about#index'
