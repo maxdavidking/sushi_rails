b6f403655e330fa86711310e0be1cd7be2a00e3a
Max King <maxdavidking@gmail.com>
2018-03-30 15:11:02 -0500

    Add code to allow users to leave orgs

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -37,6 +37,6 @@ class UserController < ApplicationController
        @user = User.find(params[:id])
     end
     def user_params
-      params.require(:user).permit(:name, :organization)
+      params.require(:user).permit(:name, :organization_id)
     end
 end
