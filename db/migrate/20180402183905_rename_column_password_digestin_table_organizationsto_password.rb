516576b9e8eb73972cad21390e8f227bceca87d8
Max King <maxdavidking@gmail.com>
2018-04-02 14:12:59 -0500

    Remove unecessary migration - rolled back

--- db/migrate/20180402183905_rename_column_password_digestin_table_organizationsto_password.rb
+++
@@ -1,5 +0,0 @@
-class RenameColumnPasswordDigestinTableOrganizationstoPassword < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :organizations, :password_digest, :password
-  end
-end
