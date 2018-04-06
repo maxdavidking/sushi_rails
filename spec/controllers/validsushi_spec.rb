ed7c193cc4aadf39776601c0ff491f984aaf7e5e
Max King <maxdavidking@gmail.com>
2018-04-06 15:56:36 -0500

    Rewrite sushi import to rely on org_id

--- spec/controllers/validsushi_spec.rb
+++ spec/controllers/validsushi_spec.rb
@@ -7,15 +7,17 @@ RSpec.describe "ValidsushiController" do
     click_link "Login"
     Validsushi.create!(name: "jstor", endpoint: "https://www.jstor.org/sushi", cust_id: "YOUR_CUSTOMER_ID", req_id: "YOUR_REQUESTOR_ID", report_start: "2017-01-01", report_end: "2017-12-31", password: "OPTIONAL")
     visit('/validsushi')
+    Organization.create!(id: 99, name: "hello123", password: "test", email: "test@example.com")
+    current_user.update(organization_id: 99)
   end
-  
+
   describe "Valid Sushi features", :type => :feature do
     include ApplicationHelper
     it "Lists all valid Sushi connections" do
       sign_in
       expect(page).to have_content("jstor")
     end
-    it "Imports valid sushi connections to user" do
+    it "Imports valid sushi connections to user through organization" do
       sign_in
       click_link("Import")
       expect(page).to have_content("jstor")
