ab2d1f72deede77a9b881f00cccff9370039c31f
Max King <maxdavidking@gmail.com>
2018-04-06 14:06:19 -0500

    Add white space for clarity

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
+  
   def current_organization
     @current_organization = current_user.organization
   end
