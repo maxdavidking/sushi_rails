2763b8ba9fbab8f919031d6dcda0c1c49b638035
Max King <maxdavidking@gmail.com>
2018-04-12 12:01:30 -0500

    Run migration rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_11_210501) do
+ActiveRecord::Schema.define(version: 2018_04_12_170014) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(version: 2018_04_11_210501) do
 
   create_table "data", force: :cascade do |t|
     t.string "date"
-    t.string "file"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "organization_id"
