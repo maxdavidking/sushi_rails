47577707c89ebb8f34e0cd9f1acf8363e2890259
Max King <maxdavidking@gmail.com>
2018-05-18 14:35:15 -0500

    Validsushi new method now creates a new record in sushi model, passing validsushi data to that record

--- app/controllers/validsushi_controller.rb
+++ app/controllers/validsushi_controller.rb
@@ -3,6 +3,11 @@ class ValidsushiController < ApplicationController
     @validsushi = Validsushi.order(:name)
   end
 
+  def new
+    @validsushi = Validsushi.find(params[:id])
+    @sushi = Sushi.new
+  end
+
   def import
     @validsushi = Validsushi.find(params[:id])
     @sushi = Sushi.new(name: @validsushi.name, endpoint: @validsushi.endpoint, cust_id: @validsushi.cust_id, req_id: @validsushi.req_id, report_start: @validsushi.report_start, report_end: @validsushi.report_end, password: @validsushi.password, organization_id: current_organization.id )
