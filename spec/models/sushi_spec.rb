a86bac1ef557aaa91d6119e5acb9ff4fb58b8727
Max King <maxdavidking@gmail.com>
2018-05-30 11:34:48 -0500

    Fixes to sync with rubocop styling specs

--- spec/models/sushi_spec.rb
+++ spec/models/sushi_spec.rb
@@ -7,6 +7,5 @@ RSpec.describe Sushi, type: :model do
   it { should validate_presence_of(:cust_id) }
   it { should validate_presence_of(:report_start) }
   it { should validate_presence_of(:report_end) }
-  it { should validate_uniqueness_of(:name).scoped_to(:organization_id)}
-
+  it { should validate_uniqueness_of(:name).scoped_to(:organization_id) }
 end
