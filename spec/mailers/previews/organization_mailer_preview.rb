229480ef82d9508a379a67090dd6ae15fe1a0a28
Max King <maxdavidking@gmail.com>
2018-05-30 11:34:21 -0500

    Remove old mailer spec

--- spec/mailers/previews/organization_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/organization_mailer
-class OrganizationMailerPreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/organization_mailer/password_reset
-  def password_reset
-    OrganizationMailerMailer.password_reset
-  end
-
-end
