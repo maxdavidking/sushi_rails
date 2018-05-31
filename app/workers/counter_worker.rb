6cba9f56eff607ca45a87d1917b4b2bf2593a5b1
Max King <maxdavidking@gmail.com>
2018-05-31 16:06:32 -0500

    Add counter worker that runs counter calls as a background process

---
+++ app/workers/counter_worker.rb
@@ -0,0 +1,17 @@
+class CounterWorker
+  include Sidekiq::Worker
+  include ApplicationHelper
+
+  def perform(sushi_id, organization_id)
+    sushi = Sushi.find(sushi_id)
+    organization = Organization.find(organization_id)
+    sushi_call(sushi)
+    months_math(sushi.report_start, sushi.report_end)
+    count_months
+    xml_open
+    get_secondary_data
+    get_item_data
+    get_total_data
+    file_type(sushi, organization)
+  end
+end
