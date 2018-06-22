c72580e67ced48d70467f808b133559faf5d6ccc
Max King <maxdavidking@gmail.com>
2018-06-22 11:15:15 -0500

    Display timestamp on user index page take it out of filename

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module SushiRails
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.time_zone = 'Central Time (US & Canada)'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
