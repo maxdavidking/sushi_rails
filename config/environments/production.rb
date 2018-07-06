d72e349f50a02ba11da9b0d796272a8447562748
Max King <maxdavidking@gmail.com>
2018-07-06 10:27:19 -0500

    Add both production and dev regex's to action cable allowed origins

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,9 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
    config.action_cable.url = ENV["CABLE_URL"]
-   config.action_cable.allowed_request_origins = [/(http|https):\/\/counter-dev.us-east-1.elasticbeanstalk.*/]
+   config.action_cable.allowed_request_origins =
+      [/(http|https):\/\/counter-dev.us-east-1.elasticbeanstalk.*/,
+     /(http|https):\/\/fetchcounter.*/ ]
 
 
 
