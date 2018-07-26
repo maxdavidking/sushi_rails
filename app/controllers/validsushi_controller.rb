a4b614a2ba3c56c12b854d8655ff8e190e117fe3
Max King <maxdavidking@gmail.com>
2018-07-26 11:27:04 -0500

    Add header response code to validsushi controller

--- app/controllers/validsushi_controller.rb
+++ app/controllers/validsushi_controller.rb
@@ -1,6 +1,9 @@
 class ValidsushiController < ApplicationController
   def new
+    # Does not correspond to a route - only used for importing data
+    # to Sushi.new on click in the view sushi/_validsushi.new.html.erb
     @validsushi = Validsushi.find(params[:id])
     @sushi = Sushi.new
+    head :ok
   end
 end
