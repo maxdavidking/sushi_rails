b6265b78db4799ef94d3a08f0f6bc3beaa4dcdc8
Max King <maxdavidking@gmail.com>
2018-07-26 11:26:04 -0500

    Update validsushi tests for controller_testing gem syntax

--- spec/controllers/validsushi_controller_spec.rb
+++ spec/controllers/validsushi_controller_spec.rb
@@ -5,7 +5,16 @@ RSpec.describe ValidsushiController do
   include ControllerHelper
   describe "GET #new" do
     it "does not render the #new template" do
-      get :new
+      validsushi = Validsushi.create!(
+        name: "fake",
+        endpoint: "https://www.jstor.org/sushi",
+        cust_id: "YOUR_CUSTOMER_ID",
+        req_id: "YOUR_REQUESTOR_ID",
+        report_start: "2017-01-01",
+        report_end: "2017-12-31",
+        password: "OPTIONAL"
+      )
+      get :new, params: { id: validsushi.id }
 
       expect(response).not_to render_template(:new)
     end
