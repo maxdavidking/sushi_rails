5b6797c7c62d3a9ae2db8e9761d5c87f8e739de9
Max King <maxdavidking@gmail.com>
2018-07-26 11:07:14 -0500

    Add test for validsushi controller

--- spec/controllers/validsushi_controller_spec.rb
+++ spec/controllers/validsushi_controller_spec.rb
@@ -1,11 +1,13 @@
 require "rails_helper"
 
-RSpec.describe "Validsushi Controller" do
-  describe "Validsushi Controller Tests" do
-    # To get access to session variables with current_user and current_org
-    include ControllerHelper
-    it "Does something" do
-      # Do something
+RSpec.describe ValidsushiController do
+  # To get access to session variables with current_user and current_org
+  include ControllerHelper
+  describe "GET #new" do
+    it "does not render the #new template" do
+      get :new
+
+      expect(response).not_to render_template(:new)
     end
   end
 end
