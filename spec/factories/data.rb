bc6eab0a9d7c2c285c0b55e80b472f7ae3a208ff
Max King <maxdavidking@gmail.com>
2018-07-31 10:41:53 -0500

    Set up factory_bot and create default factories for each model

---
+++ spec/factories/data.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :datum do
+    sushi
+    organization
+    date "2017-01-01"
+  end
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    name "test"
+    uid "test_uid"
+    provider "test_provider"
+    organization
+  end
+end
