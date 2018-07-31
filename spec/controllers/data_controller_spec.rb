2b8570b8a645860fce3810aacc4bc343402083d8
Max King <maxdavidking@gmail.com>
2018-07-31 10:44:09 -0500

    Update RSpec arrange step to work with factories

--- spec/controllers/data_controller_spec.rb
+++ spec/controllers/data_controller_spec.rb
@@ -2,19 +2,7 @@ require "rails_helper"
 
 RSpec.describe DataController do
   let(:create_datum) do
-    Organization.create!(id: 100, name: "IIT", password: "test", email: "test@example.com")
-    Sushi.create!(
-      id: 201,
-      name: "jstor",
-      endpoint: "https://www.jstor.org/sushi",
-      cust_id: "iit.edu",
-      req_id: "galvinlib",
-      report_start: "2016-01-01",
-      report_end: "2016-12-31",
-      password: "",
-      organization_id: 99
-    )
-    @file = Datum.create!(id: 101, date: "2017-01-01", organization_id: 100, sushi_id: 201)
+    @file = create(:datum)
   end
   # To get access to session variables with current_user and current_org
   include ControllerHelper
@@ -36,12 +24,14 @@ RSpec.describe DataController do
     context "when a file fails to delete" do
       it "does not delete the file" do
         create_datum
-        pending "Failure of ActiveStorage file needs to be recreated"
+        # Need to find a way to purposefully get ActiveStorage to fail
+        pending "Failure of ActiveStorage delete"
         expect(Datum.where(id: @file.id).count).to eq(1)
       end
       it "flashes a warning message" do
         create_datum
-        pending "Failure of ActiveStorage file needs to be recreated"
+        # Need to find a way to purposefully get ActiveStorage to fail
+        pending "Failure of ActiveStorage delete"
         expect(flash[:warning]).to eq("Something went wrong - #{@file} was not deleted.")
       end
     end
