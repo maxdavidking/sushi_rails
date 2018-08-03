cf820db6607a8cda48d50f450381e713a247c11b
Max King <maxdavidking@gmail.com>
2018-08-03 11:01:47 -0500

    Fix all validsushi tests and corresponding code

--- app/controllers/validsushi_controller.rb
+++ app/controllers/validsushi_controller.rb
@@ -4,6 +4,5 @@ class ValidsushiController < ApplicationController
     # to Sushi.new on click in the view sushi/_validsushi.new.html.erb
     @validsushi = Validsushi.find(params[:id])
     @sushi = Sushi.new
-    head :ok
   end
 end
--- spec/controllers/validsushi_controller_spec.rb
+++ spec/controllers/validsushi_controller_spec.rb
@@ -4,19 +4,11 @@ RSpec.describe ValidsushiController do
   # To get access to session variables with current_user and current_org
   include ControllerHelper
   describe "GET #new" do
-    it "does not render the #new template" do
-      validsushi = Validsushi.create!(
-        name: "fake",
-        endpoint: "https://www.jstor.org/sushi",
-        cust_id: "YOUR_CUSTOMER_ID",
-        req_id: "YOUR_REQUESTOR_ID",
-        report_start: "2017-01-01",
-        report_end: "2017-12-31",
-        password: "OPTIONAL"
-      )
+    it "renders the #new template" do
+      validsushi = create(:validsushi)
       get :new, params: { id: validsushi.id }
 
-      expect(response).not_to render_template(:new)
+      expect(response).to render_template(:new)
     end
   end
 end
