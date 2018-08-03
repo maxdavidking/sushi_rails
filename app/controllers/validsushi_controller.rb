46bfb6eb7565dc88115e1da1e738301178d8528d
Max King <maxdavidking@gmail.com>
2018-08-03 11:02:48 -0500

    Update comment to reflect code

--- app/controllers/validsushi_controller.rb
+++ app/controllers/validsushi_controller.rb
@@ -1,7 +1,6 @@
 class ValidsushiController < ApplicationController
   def new
-    # Does not correspond to a route - only used for importing data
-    # to Sushi.new on click in the view sushi/_validsushi.new.html.erb
+    # Autopopulates validsushi data into a Sushi.new form
     @validsushi = Validsushi.find(params[:id])
     @sushi = Sushi.new
   end
