0bf201995c1e117960afc9ba1d1bdd7c6a1f6545
Max King <maxdavidking@gmail.com>
2018-08-06 15:07:13 -0500

    Update sushi controller to pass tests

--- app/controllers/sushi_controller.rb
+++ app/controllers/sushi_controller.rb
@@ -34,8 +34,7 @@ class SushiController < ApplicationController
     @sushi = Sushi.find(params[:id])
     unless session[:user_id] == @sushi.user_id || current_organization.id = @sushi.organization_id
       flash[:danger] = "That's not your sushi connection"
-      redirect_to root_path
-      return
+      redirect_to root_path and return
     end
   end
 
@@ -54,8 +53,7 @@ class SushiController < ApplicationController
     @error = helpers.error
     unless session[:user_id] == @sushi.user_id || current_organization.id = @sushi.organization_id
       flash[:danger] = "That's not your sushi connection"
-      redirect_to root_path
-      return
+      redirect_to root_path and return
     end
   end
 
@@ -65,8 +63,7 @@ class SushiController < ApplicationController
     # Ensure user can only access their own connections
     unless session[:user_id] == @sushi.user_id || current_organization.id == @sushi.organization_id
       flash[:danger] = "That's not your sushi connection"
-      redirect_to root_path
-      return
+      redirect_to root_path and return
     end
     begin
       CounterWorker.perform_async(@sushi.id, @organization.id)
@@ -84,8 +81,7 @@ class SushiController < ApplicationController
     @sushi = Sushi.find(params[:id])
     unless current_organization.id == @sushi.organization_id
       flash[:danger] = "That's not your sushi connection"
-      redirect_to root_path
-      return
+      redirect_to root_path and return
     end
     @sushi.destroy
     respond_to do |format|
